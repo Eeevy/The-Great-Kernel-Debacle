67aa97ac3428f80029baff74398aa8d61fea027b
Evelyn Gustavsson <Evy@Evelyns-MacBook-Air.local>
2016-03-06 19:35:12 +0100

    FirstFit nästan klar! :)

--- src/memory/FirstFit.java
+++ src/memory/FirstFit.java
@@ -1,5 +1,8 @@
 package memory;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * This memory model allocates memory cells based on the first-fit method. 
  * 
@@ -8,6 +11,13 @@ package memory;
  */
 public class FirstFit extends Memory {
 
+	// nextAvailablePosition helps us keep track of where to start looking in the memory-array, 
+	// so that we do not have to go through it from the start again.
+	private int nextAvailablePosition;
+	// the start and end positions-map helps us when we want to release a pointer from the memory,
+	// and also when we want to make sure our new memory-allocations fit in the available space
+	private Map<Integer, Integer> startAndEndPositions;
+	
 	/**
 	 * Initializes an instance of a first fit-based memory.
 	 * 
@@ -16,6 +26,8 @@ public class FirstFit extends Memory {
 	public FirstFit(int size) {
 		super(size);
 		// TODO Implement this!
+		this.nextAvailablePosition = 0;
+		this.startAndEndPositions = new HashMap<Integer, Integer>();
 	}
 
 	/**
@@ -27,6 +39,35 @@ public class FirstFit extends Memory {
 	@Override
 	public Pointer alloc(int size) {
 		// TODO Implement this!
+		for (int i = this.nextAvailablePosition; i < this.cells.length; i++) {
+			if (this.cells[i] == 0) {
+				// we found an empty memory cell
+				boolean isAvailable = true;
+				for (int j = i; j <= size+i; j++){
+					// checking to see that there is room enough to allocate something of @size (this could be improved)
+					if (this.cells[j] != 0){
+						// there wasn't room enough, now we start to look for the next empty memory cell available
+						while(this.cells[j] != 0) {
+							// we set j to the end position of the occupied space
+							j = j+this.startAndEndPositions.get(j);
+						}
+						i = j-1; // set i to the next available memory cell 
+						isAvailable = false;
+						break;
+					}
+				}
+				
+				if (isAvailable) {
+					this.nextAvailablePosition = i+size;
+					Pointer p = new Pointer(i, this);
+					this.startAndEndPositions.put(i, size);
+					return p;
+				}
+				else{
+					continue;
+				}			
+			}
+		}
 		return null;
 	}
 	
@@ -38,6 +79,14 @@ public class FirstFit extends Memory {
 	@Override
 	public void release(Pointer p) {
 		// TODO Implement this!
+		int startPosition = p.pointsAt();
+		int length = this.startAndEndPositions.get(startPosition).intValue();
+		for (int i = 0; i < length; i++){
+			this.cells[startPosition+i] = 0;
+		}
+		
+		this.startAndEndPositions.remove(startPosition);
+		this.nextAvailablePosition = startPosition;
 	}
 	
 	/**
@@ -51,6 +100,7 @@ public class FirstFit extends Memory {
 	@Override
 	public void printLayout() {
 		// TODO Implement this!
+		System.out.println(this.toString());
 	}
 	
 	/**
